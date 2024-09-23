600f296ccef9eb3b8ced69d16e9a7f3b758a47a0
Abdulrahman Al-M <avoided.sy@gmail.com>
2024-09-23 21:05:05 +0300

    Show loading skeletons

---
+++ src/components/GaemCardSkeleton.tsx
@@ -0,0 +1,15 @@
+import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
+import React from "react";
+
+const GaemCardSkeleton = () => {
+  return (
+    <Card w="300px" borderRadius={10} overflow="hidden">
+      <Skeleton w="300px" h="200px" />
+      <CardBody>
+        <SkeletonText />
+      </CardBody>
+    </Card>
+  );
+};
+
+export default GaemCardSkeleton;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,9 +1,11 @@
 import { SimpleGrid } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
+import GaemCardSkeleton from "./GaemCardSkeleton";
 
 const GameGrid = () => {
-  const { games, error } = useGames();
+  const { games, error, isLoading } = useGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
     <>
@@ -13,6 +15,8 @@ const GameGrid = () => {
         spacing={7}
         padding="10px"
       >
+        {isLoading &&
+          skeletons.map((skeletons) => <GaemCardSkeleton key={skeletons} />)}
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
