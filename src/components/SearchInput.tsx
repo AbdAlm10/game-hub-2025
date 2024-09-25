d6ef9817bfec58bd48905b86fb080165f7ca3d08
Abd-Alm <avoided.sy@.com>
2024-09-25 15:32:52 +0300

    Impelement searching

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,17 +1,31 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
-import React from "react";
+import { FormEvent, useRef } from "react";
 import { BsSearch } from "react-icons/bs";
 
-const SearchInput = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const SearchInput = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  const handleSearching = (e: FormEvent) => {
+    e.preventDefault();
+    if (ref.current) onSearch(ref.current.value);
+  };
+
   return (
-    <InputGroup>
-      <InputLeftElement children={<BsSearch />} />
-      <Input
-        borderRadius={20}
-        placeholder="Search games ..."
-        variant="filled"
-      />
-    </InputGroup>
+    <form onSubmit={(e) => handleSearching(e)}>
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input
+          ref={ref}
+          borderRadius={20}
+          placeholder="Search games ..."
+          variant="filled"
+        />
+      </InputGroup>
+    </form>
   );
 };
 
