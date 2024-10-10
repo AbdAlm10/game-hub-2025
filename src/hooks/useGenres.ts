11107f9245174ad0817573a7af004f4919237012
Abd-Alm <avoided.sy@.com>
2024-10-10 21:38:43 +0300

    Refactor entity interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,7 +2,7 @@ import { useQuery } from "react-query";
 import APIClient from "../services/api-client";
 import Genres from "../data/Genres";
 import ms from "ms";
-import { Genre } from "../entities/Genre";
+import Genre from "../entities/Genre";
 
 const apiClient = new APIClient<Genre>("/genres");
 
--- src/hooks/useScreenShoots.ts
+++ src/hooks/useScreenShoots.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "react-query";
-import { Screenshot } from "../entities/screenshot";
+import Screenshot from "../entities/screenshot";
 import APIClient from "../services/api-client";
 
 const useScreenShoots = (gameId: number) => {
